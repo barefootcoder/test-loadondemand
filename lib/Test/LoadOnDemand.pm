74e1ae9100bdb4aae2f90bf732056891a09a14e4
Buddy Burden <barefootcoder@gmail.com>
2018-08-28 02:38:14 -0700

    skeleton distro

---
+++ lib/Test/LoadOnDemand.pm
@@ -0,0 +1,28 @@
+package Test::LoadOnDemand;
+
+use strict;
+use warnings;
+use autodie ':all';
+
+# VERSION
+
+
+
+1;
+
+
+
+# ABSTRACT: test whether calling a function loads a module
+# COPYRIGHT
+
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 USAGE
+
+
+=head1 BUGS, CAVEATS and NOTES
